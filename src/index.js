b02419087a08a85fce6f0b5c08b8a5c41f629d15
Brian Bycynski <high5bri@hotmail.com>
2017-09-26 05:59:58 -0400

    the color guessing game

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
 import './styles/screen.scss'
 
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
+//
+// document.addEventListener('DOMContentLoaded', main)
+
+var colors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(255, 0, 255)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 0, 255)',
+  'rgb(20, 120, 40)'
+]
+
+var squares = document.querySelectorAll('.square')
+var pickedColor = pickColor()
+var colorDisplay = document.getElementById('colorDisplay')
+var messageDisplay = document.querySelector('#message')
+
+colorDisplay.textContent = pickedColor
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i]
+  squares[i].addEventListener('click', function () {
+    var clickedColor = this.style.backgroundColor
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = 'Correct'
+      changeColors(clickedColor)
+    } else {
+      this.style.backgroundColor = '#232323'
+      messageDisplay.textContent = 'Try Again'
+    }
+  })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+function changeColors (color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color
+  }
+}
+
+function pickColor () {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random]
+}
 
 // HERE BE DRAGONS... and webpack. Don't touch.
 if (process.env.NODE_ENV !== 'production') require('./index.html')
