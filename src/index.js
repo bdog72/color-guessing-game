1f5df6a7d5d288a39546381ac944c31afa68b966
Brian Bycynski <high5bri@hotmail.com>
2017-09-27 05:32:08 -0400

    finished color guessing game

--- src/index.js
+++ src/index.js
@@ -6,19 +6,66 @@ import './styles/screen.scss'
 //
 // document.addEventListener('DOMContentLoaded', main)
 
-var colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(255, 0, 255)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 0, 255)',
-  'rgb(20, 120, 40)'
-]
+// var colors = [
+//   'RGB(255, 0, 0)',
+//   'RGB(255, 255, 0)',
+//   'RGB(255, 0, 255)',
+//   'RGB(0, 255, 0)',
+//   'RGB(0, 0, 255)',
+//   'RGB(20, 120, 40)'
+// ]
+var numSquares = 6
+var colors = generateRandomColors(numSquares)
 
 var squares = document.querySelectorAll('.square')
 var pickedColor = pickColor()
 var colorDisplay = document.getElementById('colorDisplay')
 var messageDisplay = document.querySelector('#message')
+var h1 = document.querySelector('h1')
+var resetButton = document.querySelector('#reset')
+var easyBtn = document.querySelector('#easyBtn')
+var hardBtn = document.querySelector('#hardBtn')
+
+easyBtn.addEventListener('click', function () {
+  hardBtn.classList.remove('selected')
+  easyBtn.classList.add('selected')
+  numSquares = 3
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i]
+    } else {
+      squares[i].style.display = 'none'
+    }
+  }
+})
+
+hardBtn.addEventListener('click', function () {
+  hardBtn.classList.add('selected')
+  easyBtn.classList.remove('selected')
+  numSquares = 6
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i]
+    squares[i].style.display = 'block'
+  }
+})
+
+resetButton.addEventListener('click', function () {
+  colors = generateRandomColors(numSquares)
+  pickedColor = pickColor()
+  colorDisplay.textContent = pickedColor
+  this.textContent = 'New Colors'
+  messageDisplay.textContent = ''
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i]
+  }
+  h1.style.backgroundColor = 'steelblue'
+})
 
 colorDisplay.textContent = pickedColor
 
@@ -28,7 +75,9 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = 'Correct'
+      resetButton.textContent = 'PLAY AGAIN'
       changeColors(clickedColor)
+      h1.style.backgroundColor = clickedColor
     } else {
       this.style.backgroundColor = '#232323'
       messageDisplay.textContent = 'Try Again'
@@ -45,6 +94,21 @@ function changeColors (color) {
 function pickColor () {
   var random = Math.floor(Math.random() * colors.length)
   return colors[random]
+}
+
+function generateRandomColors (num) {
+  var arr = []
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+  return arr
+}
+
+function randomColor () {
+  var r = Math.floor(Math.random() * 256)
+  var g = Math.floor(Math.random() * 256)
+  var b = Math.floor(Math.random() * 256)
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')'
 }
 
 // HERE BE DRAGONS... and webpack. Don't touch.
